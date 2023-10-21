99f6ddfd00074351c169904d910489b9485ff240
Student <ehuang6@mail.sfsu.edu>
2023-10-21 13:08:51 -0700

    Added member names and IDs to headers

--- b_io.c
+++ b_io.c
@@ -1,9 +1,9 @@
 /**************************************************************
-* Class:  CSC-415-0# Fall 2021
-* Names: 
-* Student IDs:
-* GitHub Name:
-* Group Name:
+* Class:  CSC-415-03 Fall 2023
+* Names: Edmund Huang, Jimmy Pan, Juan Estrada, Kripa Pokhrel
+* Student IDs: 918426293, 920950183, 923058731, 922961998
+* GitHub Name: EdmUUUndo
+* Group Name: HumanOS
 * Project: Basic File System
 *
 * File: b_io.c
