a55ac0e48dbe351849c5035ff0052dba71e1ce96
parallels <juan5801331@gmail.com>
2023-11-05 12:34:54 -0800

    Seudocode functions fs_mkdir fs_isDir fs_isFile

--- mfs.c
+++ mfs.c
@@ -13,3 +13,21 @@
  *
  * Description: functions to handle shell commands
  **************************************************************/
+
+int fs_mkdir(const char *pathname, mode_t mode){
+    //Ensure input parameter are valid, name is not empty and follows conventions
+    //Find free blocks getfreeblocks()
+    //Creat Directory Entry ()
+    //Update parent directory
+    //writing to second storage LBAwrite
+    
+
+
+}
+
+int fs_isDir(const char pathname){
+    //Ensure input is not empty
+    //Traverse the path and check if each of the components exists in the directory
+    //Once reach the specified path  check type of the directory 
+}
+
